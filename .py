37da41b107d43206b5046778c63fb40135b6d5d2
MadaMun <94043213+MadaMun@users.noreply.github.com>
2022-07-25 16:40:07 +0700

    123

---
+++ .py
@@ -0,0 +1,13 @@
+"""so hard"""
+def main():
+    """very hard"""
+    kaet_t1 = float(input())
+    if kaet_t1 < 1:
+        print("I'm so sad.")
+    elif kaet_t1 < 2:
+        one = 2 - kaet_t1
+        ans = one + 2
+        print("%.2f" %(ans))
+    elif kaet_t1 <= 4:
+        print("I'm so happy.")
+main()
